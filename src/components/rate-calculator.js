bac30e6a8bd48046944554c3a36f331d03a7447f
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-05-15 22:21:23 -0500

    Hourly rate calculator

--- src/components/rate-calculator.js
+++ src/components/rate-calculator.js
@@ -6,16 +6,32 @@ import Output from './output';
 export default class RateCalculator extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            dayRate: 300,
+            hours: 8
+        }
+    }
+
+    setDayRate(dayRate) {
+        this.setState({
+            dayRate
+        });
+    }
+
+    setHours(hours) {
+        this.setState({
+            hours
+        });
     }
 
     render() {
-        const rate = 10;
+        const rate = this.state.dayRate / this.state.hours;
         return (
             <form>
-                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} />
-                <NumberInput id="hours" label="Hours" min={1} max={12} />
+                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} value={this.state.dayRate} onChange={(dayRate) => this.setDayRate(dayRate)} />
+                <NumberInput id="hours" label="Hours" min={1} max={12} value={this.state.hours} onChange={(hours) => this.setHours(hours)} />
                 <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}
-/>
+                />
             </form>
         );
     }
